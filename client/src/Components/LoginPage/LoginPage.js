e39e0302a291906b7f1af2371b2d405dd156d10a
Sam Bell <sambelldev@tuta.io>
2021-10-18 16:40:50 +0100

    basic login post functionality - needs backend route to receive

--- client/src/Components/LoginPage/LoginPage.js
+++ client/src/Components/LoginPage/LoginPage.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import './LoginPage.css'
 
 const LoginPage = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const url = 'postRoute'
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        }
+        fetch(url, requestOptions)
+            .then(response => console.log('Submitted successfully'))
+            .catch(error => console.log('Form submit error', error))
+    }
+
     return (
         <div className="loginContainer">
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} value={email} />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -17,7 +43,7 @@ const LoginPage = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} value={password} />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
